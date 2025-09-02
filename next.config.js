14bfd9fd7aafbc84d7c4271f16154ab9370e66db
aaqibsheikh <aaqibnadeem123@gmail.com>
2025-09-03 00:46:34 +0500

    config fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // App directory is now stable in Next.js 14, no need for experimental flag
+  
+  // Note: Cache-bust parameters are part of Next.js 14's development mode
+  // and cannot be easily disabled without breaking functionality
 }
 
 module.exports = nextConfig
